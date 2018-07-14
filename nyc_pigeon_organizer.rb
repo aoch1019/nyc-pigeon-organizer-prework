28d263ce677aa448177cc427978cf05c7888b829
Andrew Och <andrewoch95@gmail.com>
2018-07-14 00:19:21 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,11 +7,11 @@ def nyc_pigeon_organizer(data)
     hash.each do |attribute, names|
       names.each do |name|
         if new_hash.include?(name) && new_hash[name].include?(category)
-          new_hash[name][category].push(attribute)
+          new_hash[name][category].push(attribute.to_s)
         elsif new_hash.include?(name)
-          new_hash[name][category] = [attribute]
+          new_hash[name][category] = [attribute.to_s]
         else
-          new_hash[name] = {category => [attribute]} 
+          new_hash[name] = {category => [attribute.to_s]} 
         end
       end
     end
